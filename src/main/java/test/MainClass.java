5b618c2e98209508c2c57f92d0b751d4b56271bd
rym razgui <rymrazgui15@gmail.com>
2024-03-06 00:54:40 +0100

    métier 3.0 + API 2.0

--- src/main/java/test/MainClass.java
+++ src/main/java/test/MainClass.java
@@ -1,28 +1,10 @@
 package test;
+import controllers.QrCode;
 
-import models.Badge;
-import models.Certificat;
-import services.ServiceBadge;
-import services.ServiceCertificat;
-import utils.DBConnection;
-
-import java.sql.Date;
-import java.sql.SQLException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-
-
-import models.Certificat;
-import services.ServiceCertificat;
-import utils.DBConnection;
-
-import java.sql.SQLException;
-import java.util.List;
 
 public class MainClass {
     public static void main(String[] args) {
-
+        QrCode.generateQRCode();
     }
 }
 
