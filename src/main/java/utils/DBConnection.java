3d8f614c608f277f756d8e0cdc185ddbced2ca55
issam-guichana <94192951+issam-guichana@users.noreply.github.com>
2024-03-06 18:07:25 +0100

    Revert "Rahma"

--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class DBConnection {
     private static final String URL= "jdbc:mysql://localhost:3306/formini.tn";
     private static final String USER= "root";
